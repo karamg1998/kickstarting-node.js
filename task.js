f8ba0ea35d1aaf3d0f77f045bff150e6060b1b46
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-11 17:59:32 +0530

    task 8 using form action

--- task.js
+++ task.js
@@ -5,29 +5,18 @@ const server = http.createServer((req, res) => {
   const url = req.url;
  
 
-  if (url === '/home' ) {
-    res.setHeader('Content-Type','text/html');
+  if (url === '/' ) {
     res.write('<html>')
     res.write('<head><title>Welcome home</title><head>');
-    res.write('<body><h1>Welcome home</h1></body>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name ="message"><button type="submit">Send</button></form></body>')
     res.write('<html>');
-    res.end();
+    return res.end();
   }
-  if (url === '/about') {
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>')
-    res.write('<head><title>Welcome about</title><head>');
-    res.write('<body><h1>Welcome to About Us page</h1></body>')
-    res.write('<html>');
-    res.end();
-  }
-  if (url === '/node') {
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>')
-    res.write('<head><title>Welcome node</title><head>');
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    res.write('<html>');
-    res.end();
-  }
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>my first node server</title></head>')
+  res.write('<body><h1>hello my first node.js server</h1></body>')
+  res.write('</html>')
+  res.end();
 });
-  server.listen(1000);
+  server.listen(5000);
