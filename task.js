9b8b87356e0dab6e0ba29c0c24114ac301aac1e3
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-11 18:09:27 +0530

    task 8 statuscode

--- task.js
+++ task.js
@@ -1,15 +1,23 @@
 const http = require('http');
-
+const fs=require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
- 
+ const method=req.method;
 
   if (url === '/' ) {
     res.write('<html>')
     res.write('<head><title>Welcome home</title><head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name ="message"><button type="submit">Send</button></form></body>')
     res.write('<html>');
+    return res.end();
+  }
+
+  if(url==='/message' && method ==='POST')
+  {
+    fs.writeFileSync('message.txt','dummy');
+    res.statusCode=302;//used for redirection
+    res.setHeader('location','/');
     return res.end();
   }
   res.setHeader('Content-Type','text/html');
@@ -19,4 +27,4 @@ const server = http.createServer((req, res) => {
   res.write('</html>')
   res.end();
 });
-  server.listen(5000);
+  server.listen(7000);
