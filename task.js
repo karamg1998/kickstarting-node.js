94f37e42d8c554c3722d7b774e5a91895054b618
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-11 15:44:57 +0530

    task 7 question 2 custom response

--- task.js
+++ task.js
@@ -1,14 +1,33 @@
-let http = require('http');
+const http = require('http');
 
 
-const server=http.createServer((req,res)=>{
-  console.log(req.url,req.method,req.headers);
-  res.setHeader('Content-Type','text/html');
-  res.write('<html>');
-  res.write('<head><title>my first node server</title></head>')
-  res.write('<body><h1>hello my first node.js server</h1></body>')
-  res.write('</html>')
-  res.end();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+ 
+
+  if (url === '/home' ) {
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>Welcome home</title><head>');
+    res.write('<body><h1>Welcome home</h1></body>')
+    res.write('<html>');
+    res.end();
+  }
+  if (url === '/about') {
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>Welcome about</title><head>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>')
+    res.write('<html>');
+    res.end();
+  }
+  if (url === '/node') {
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>Welcome node</title><head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    res.write('<html>');
+    res.end();
+  }
 });
-
-server.listen(4000);
+  server.listen(1000);
