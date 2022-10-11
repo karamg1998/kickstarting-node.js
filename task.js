85791527fcfa4761bc1e163a679c99047d858eb4
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-11 15:09:50 +0530

    task 7 question 1

--- task.js
+++ task.js
@@ -1,7 +1,14 @@
-let http=require('http');
+let http = require('http');
 
-const server= http.createServer((get,send)=>{
-    console.log('karamveer');
+
+const server=http.createServer((req,res)=>{
+  console.log(req.url,req.method,req.headers);
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>my first node server</title></head>')
+  res.write('<body><h1>hello my first node.js server</h1></body>')
+  res.write('</html>')
+  res.end();
 });
 
 server.listen(4000);
